8f48f7bce06e2d5017b9194a4093d727ce5da50d
Espenk <espenkleivane@gmail.com>
2026-02-18 13:33:54 +0100

    game

--- bash/scripts/rpg_game.sh
+++ bash/scripts/rpg_game.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
+#functions
+
+player_health=100
+player_attack=40
+
+run_game() {
+        # clear the screen from old cmds
+        clear
+        echo "You have entred the area"
+        local playing=true
+        while [ "$playing" == "true" ]; do
+                # game loop
+                read -p "(game) what will you do (return, attack): " game_input
+                if [ "$game_input" == "attack" ]; then
+                        echo "Attack troll"
+                elif [ "$game_input" == "return" ]; then
+                        playing=false
+                else
+                        echo "No game command for $game_input"
+                fi
+        done
+}
 echo "RPG world"
 version=1.0
 echo "Version $version"
 
 # game loop
 while true; do
-        read -p "Enter something (type false to stop): " userinput
-
-        if [ "$userinput" == "false" ]; then
-                echo "Quitting game"
-                break
+        read -p "Enter something (type quit, new or load): " user_input
+        if [ -z "$user_input" ]; then
+                echo "Please type a command!"
+                continue # Skips the rest of the loop and starts over
         fi
 
-        echo "You entered $userinput"
+        if [ "$user_input" == "quit" ]; then
+                echo "Quitting game"
+                break
+        elif [ "$user_input" == "new" ]; then
+                echo "Starting a new game"
+                run_game
+        else
+                echo "No game command for $user_input"
+        fi
+
+        echo "You entered $user_input"
 done
