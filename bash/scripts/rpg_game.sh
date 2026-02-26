9b49e43abf688b6ade42b75dacda19316750df76
Espenk <espenkleivane@gmail.com>
2026-02-26 21:05:10 +0100

    bash

--- bash/scripts/rpg_game.sh
+++ bash/scripts/rpg_game.sh
@@ -1,48 +1,90 @@
 #!/bin/bash
 
-#functions
-
+# --- VARIABLES ---
+# Rule: We use 'var=value' (no $) to ASSIGN or CHANGE a variable.
+# Rule: We use '$var' to READ or ACCESS the value stored inside it.
 player_health=100
 player_attack=40
 
+declare -A enemys
+enemys[ogre]=100
+enemys[demon]=120
+enemys[troll]=130
+
+roll_dice() {
+ local min=2
+ local max=50
+ # Inside $(( )), Bash is smart enough to know you mean variables,
+ # so you can omit the $ (though $min still works).
+ echo $(( (RANDOM % 49) + min))
+}
+
 run_game() {
-        # clear the screen from old cmds
-        clear
-        echo "You have entred the area"
+        echo "You have entered the area"
+        local names=("${!enemys[@]}")
+        local ran=$(( RANDOM % ${#enemys[@]} ))
+        local enemy_name="${names[$ran]}"
+        local enemy_hp="${enemys[$enemy_name]}"
+
         local playing=true
         while [ "$playing" == "true" ]; do
-                # game loop
-                read -p "(game) what will you do (return, attack): " game_input
-                if [ "$game_input" == "attack" ]; then
-                        echo "Attack troll"
-                elif [ "$game_input" == "return" ]; then
+                # --- WHY [ ] vs [[ ]] ---
+                # [ ] is the "Old School" way. It's picky and needs quotes around variables.
+                # [[ ]] is the "Modern" way. It handles empty variables and OR/AND logic
+                # much better without crashing. It is generally safer for strings.
+
+                if [ $enemy_hp -le 0 ]; then
+                 echo "$enemy_name is dead..., you win!"
+                 playing=false
+                 continue
+                else
+                 echo "You will fight $enemy_name with health $enemy_hp"
+                fi
+
+                read -p "(game) what will you do (run / r, attack / a): " game_input
+
+                # We use [[ ]] here because we are doing complex "OR" (||) logic.
+                # Single brackets [ ] struggle with || inside them.
+                if [[ "$game_input" == "attack" || "$game_input" == "a" ]]; then
+                        echo "Attack $enemy_name"
+                        damage=$(roll_dice)
+
+                        # We use enemy_hp (no $) to tell Bash WHICH variable to update.
+                        # We use enemy_hp and damage (no $) inside (( )) because it's a math context.
+                        enemy_hp=$(( enemy_hp - damage ))
+
+                        if [ $enemy_hp -gt 1 ]; then
+                         echo "$enemy_name lost $damage and has now $enemy_hp left"
+                        fi
+                elif [[ "$game_input" == "run" || "$game_input" == "r" ]]; then
                         playing=false
                 else
                         echo "No game command for $game_input"
                 fi
         done
 }
+
+## game main
 echo "RPG world"
 version=1.0
 echo "Version $version"
 
-# game loop
 while true; do
-        read -p "Enter something (type quit, new or load): " user_input
+        read -p "Enter something (type quit / q, new / n or load / l): " user_input
+
+        # [ -z ] checks if a string is empty.
         if [ -z "$user_input" ]; then
                 echo "Please type a command!"
-                continue # Skips the rest of the loop and starts over
+                continue
         fi
 
-        if [ "$user_input" == "quit" ]; then
+        if [[ "$user_input" == "quit" || "$user_input" == "q" ]]; then
                 echo "Quitting game"
                 break
-        elif [ "$user_input" == "new" ]; then
+        elif [[ "$user_input" == "new" || "$user_input" == "n" ]]; then
                 echo "Starting a new game"
                 run_game
         else
                 echo "No game command for $user_input"
         fi
-
-        echo "You entered $user_input"
 done
--- bash/scripts/run_bash_script_101.sh
+++ bash/scripts/run_bash_script_101.sh
@@ -5,4 +5,86 @@
 # sudo chmod +x run_bash_script_101.sh
 # sudo ./run_bash_script_101.sh
 
-echo "101"
+echo "Hello 101"
+echo "Hello 102"
+echo "use ; for multiple lines"; echo " like this"
+
+name="World"
+echo " Hello $name"
+
+current_dir=$(pwd)
+echo $current_dir
+
+number=42
+echo $number
+
+echo "Path is $PATH"
+
+my_function() {
+ local local_var="I am local"
+ echo $local_var
+}
+my_function
+
+player="John"
+game="RPG"
+echo $player$game
+
+health=80
+candybar=22
+sum=$((health + candybar))
+echo $sum
+
+greeting="Allo, allo"
+name="Steve"
+welcome="$greeting, $name"
+echo $welcome
+
+num1=5
+num2=2
+sum=$((num1 + num2))
+diff=$((num1 - num2))
+mult=$((num1 * num2))
+div=$((num1 / num2))
+echo "sum $sum, diff $diff, mult $mult, div $div"
+
+enemy=("ogre" "demon" "troll")
+for e in "${enemy[@]}"; do
+ echo $e
+done
+
+declare -A enemys
+enemys[ogre]="100kg"
+enemys[demon]="150kg"
+enemys[troll]="200kg"
+enemys[god]="50kg"
+unset enemys[god]
+echo ${enemys[demon]}
+
+num=15
+if [ $num -gt 10 ]; then
+ echo "Number is greater then 10 $num"
+fi
+
+num2=2
+if [ $num2 -gt 10 ]; then
+ echo "Number is greater then 10 $num2"
+else
+ echo "Number is not greater then 10 $num2"
+fi
+
+num3=20
+if [ $num3 -gt 20 ]; then
+ echo "Number is greater then 20 $num3"
+elif [ $num3 -eq 20 ]; then
+ echo "Number is equal to 20 $num3"
+else
+ echo "Number is less then 20 $num3"
+fi
+
+num4=32
+if [ $num4 -gt 20 ]; then
+ if [ $num4 -lt 35 ]; then
+  echo "Number is gt then 20 and less then 35 $num4"
+ fi
+fi
