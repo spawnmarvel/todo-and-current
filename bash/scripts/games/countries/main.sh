e56db621ac39fc5fe8a6eadc4a2e844909a7612a
Espenk <espenkleivane@gmail.com>
2026-04-03 20:57:42 +0200

    key

---
+++ bash/scripts/games/countries/main.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+
+# --- MAIN MENU ---
+echo "Countries"
+echo "Inspired by https://web.mit.edu/mprat/Public/web/Terminus/Web/main.html"
+version=1.0
+echo "Version $version"
+echo ""
+echo "Fly to countries from home"
+echo "It might not be direct flights to your destination, then select a valid destination"
+echo "You might be stopped by customs or other's on your travell"
+echo "As you travell, you can store notes in your notebook, you might need them for later" 
+echo ""
+echo "Type m for menu"
+
+fly_countries=("Norway" "Sweden" "Denmark")
+
+fun_learning() {
+    echo "What did i learn making this or where did i send time debugging?"
+    echo "1. bash is pickey about spaces, it needs them."
+}
+fun_menu() {
+   echo "Menu: (ls: look), (cd destination), (nano notebook), (q quit), (m menu):"
+}
+fun_store_notebook() {
+    echo "$1"
+    echo "Must implement notebook, save and load"
+}
+fun_fly_from_home() {
+  echo "Looking for flights from home:"
+  echo "${fly_countries[@]}"
+
+}
+fun_destination_move() {
+  local move_to_destination
+  local can_fly=false
+  # --- FUNCTION ARGUMENTS ($1, $2) ---
+    # We check if $1 (the first argument) is NOT empty using [[ -n ]].
+    # [[ ]] is modern and safer for string checks than [ ].
+    if [[ -n "$1" ]]; then
+        move_to_destination=$1
+        echo "Travelling to $move_to_destination"
+        for c in "${fly_countries[@]}"; do
+          if [[ "$1" == "$c" ]]; then
+            can_fly=true
+            break
+          fi
+        done
+        if [[ "$can_fly" == true ]]; then
+          if [[ "$1" == "Norway" ]]; then
+            echo "Domestic flight? You are in norway"
+          fi
+          echo "You can fly to $1"
+        else
+          echo "You can not fly to $1. It is not possible from where you are"
+          echo "Mabye dived up the flights, there are more planes from Denmark"
+        fi
+    else
+        echo "Missing destination"
+    fi
+}
+while true; do
+    read -p ">" tmp_user_input tmp_destination
+    # to lower
+    user_input="${tmp_user_input,,}"
+
+    # [ -z ] is "Is Zero" — checks if the user just hit Enter without typing.
+    if [ -z "$user_input" ]; then 
+       continue; 
+    fi
+
+    if [[ "$user_input" == "q" ]]; then
+        echo "Quitting Capitals"
+        fun_learning
+        break
+    elif [[ "$user_input" == "nano" ]]; then
+        fun_store_notebook
+
+    elif [[ "$user_input" == "m" ]]; then
+        fun_menu
+
+    elif [[ "$user_input" == "ls" ]]; then
+        fun_fly_from_home
+
+    elif [[ "$user_input" == "cd" ]]; then
+        # [ -z ] is "Is Zero" — checks if the user just hit Enter without typing.
+        if [ -z "$tmp_destination" ]; then 
+          echo "No input for destination"
+        else
+           fun_destination_move "$tmp_destination"
+        fi
+       
+
+    else
+        echo "Unknown command."
+    fi
+done
