1e2ee2463d89395cc2180223a3be86367eb3620e
Espenk <espenkleivane@gmail.com>
2026-06-03 15:58:00 +0200

    bash

--- pki_store/pki_store_minmal_root_and_server_cert_client_server_auth/2_ps1_verify.ps1
+++ pki_store/pki_store_minmal_root_and_server_cert_client_server_auth/2_ps1_verify.ps1
@@ -9,3 +9,12 @@ openssl x509 -in C:\temp\rootCA.crt -noout -subject -dates
 openssl x509 -in C:\temp\server.crt -noout -subject -dates -purpose
 
 
+# Server Asset Verification One-Liner
+openssl x509 -noout -modulus -in C:\temp\server.crt | openssl sha256 && openssl rsa -noout -modulus -in C:\temp\server.key | openssl sha256
+
+# (stdin)= 6d6ab7e5d5d2ff8c6b0bd5ca28baea5eadb294584f0beeeb9b53a2c45d085a50
+# (stdin)= 6d6ab7e5d5d2ff8c6b0bd5ca28baea5eadb294584f0beeeb9b53a2c45d085a50
+
+# Line 1: The fingerprint of your server certificate's public key component.
+# Line 2: The fingerprint of your private key's modulus.
+# If the two alpha-numeric strings match exactly, your key pair matches.
