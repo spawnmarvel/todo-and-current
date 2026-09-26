bfaa8529b49cb6a751c5353d6a18ba6d58f2ee7b
Espenk <espenkleivane@gmail.com>
2026-09-26 23:39:06 +0200

    py

--- python/boilerplate/worker_api.py
+++ python/boilerplate/worker_api.py
@@ -1,4 +1,6 @@
 
 class ApiWorker:
-    """"""
+    """Worker class for interacting with a generic API."""
     pass
+
+    # connect to the api and do a tag count, then return the count to the controller and send it to zabbix trap
